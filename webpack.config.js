2cb9d20fb4048a47b152def82f2360a3891c4777
jyschwrtz <jyschwrtz@gmail.com>
2017-12-04 18:07:47 -0800

    refactor everything into classes (OOP)

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   // context: __dirname,
-  entry: "./lib/entry.js",
+  entry: "./lib/guitar_hero.js",
   // target: 'node',
   output: {
     // path: path.resolve(__dirname, 'lib'),
